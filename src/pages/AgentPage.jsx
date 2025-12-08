562458c5435f2cf461327fd1bd5a09d8b573c507
ggorilla11-hub <ggorilla11@gmail.com>
2025-12-08 15:15:42 +0900

    Update AgentPage.jsx

--- src/pages/AgentPage.jsx
+++ src/pages/AgentPage.jsx
@@ -71,7 +71,7 @@ function AgentPage({ user }) {
               type: 'server_vad',
               threshold: 0.5,
               prefix_padding_ms: 300,
-              silence_duration_ms: 800
+              silence_duration_ms: 1500
             }
           }
         }));
